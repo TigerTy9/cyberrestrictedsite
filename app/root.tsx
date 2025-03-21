35179e4728d2cc56af8b76630e642a2bb2146d70
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 16:42:46 -0400

    t

--- app/root.tsx
+++ app/root.tsx
@@ -89,24 +89,11 @@
 // //     </main>
 // //   );
 // // }
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
-import "./app.css";
-import "./styles/global.css";
+import { BrowserRouter } from 'react-router-dom';
 
-export default function Root() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+ReactDOM.render((
+  <BrowserRouter>
+    <App /> {/* The various pages will be displayed by the `Main` component. */}
+  </BrowserRouter>
+  ), document.getElementById('root')
+);
