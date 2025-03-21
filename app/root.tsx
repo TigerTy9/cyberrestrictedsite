9deeecd6f64305881cbc409cb4074d7d54e560e4
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 16:57:49 -0400

    t

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,7 @@ import Books from "./Books";
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
