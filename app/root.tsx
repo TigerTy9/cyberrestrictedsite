c4f2e5774c67ce4170f8de8635124e73034d99a5
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 18:17:44 -0400

    s

--- app/root.tsx
+++ app/root.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
 import Home from "./components/Home.jsx";
 //import About from "./components/About.jsx";
 import ErrorPage from "./components/ErrorPage.jsx";
