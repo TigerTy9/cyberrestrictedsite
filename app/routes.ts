03ba7ff00ab07491a4da93fa0e4860afd6d5192b
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 17:15:43 -0400

    s

--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,3 @@
-// File: app/routes.ts
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
+import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [
-  index("routes/_index.tsx"), // Home route
-  route("@/routes/books.tsx"),   // Lore route
-  // Add more routes like: route("routes/yourpage.tsx"),
-] satisfies RouteConfig;
-
-console.log("Routes loaded:", routes);
+export default [index("routes/books.tsx")] satisfies RouteConfig;
