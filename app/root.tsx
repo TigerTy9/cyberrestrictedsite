1f448aa942bc8567a61fff5345d7891caf1d4bbf
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 18:11:12 -0400

    s

--- app/root.tsx
+++ app/root.tsx
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
-    errorElement: <ErrorPage />,
+  //  errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
