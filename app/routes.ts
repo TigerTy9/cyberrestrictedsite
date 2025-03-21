63b6d762931d2b448785c7e313b3c591b6af8b0c
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 17:22:12 -0400

    s

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,14 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { index } from "@react-router/dev/routes";
+import type { RouteConfig } from "@react-router/dev/routes";
 
-export default [index("routes/books.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home.tsx"),
+  {
+    path: "about",
+    file: "routes/about.tsx",
+  },
+  {
+    path: "books",
+    file: "routes/books.tsx",
+  },
+] satisfies RouteConfig;
