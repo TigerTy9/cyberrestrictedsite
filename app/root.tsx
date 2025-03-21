fe97945b6bc66eac8bd0528456a563b6c815d325
Ty Thomasson <tythomasson@outlook.com>
2025-03-21 18:10:43 -0400

    s

--- app/root.tsx
+++ app/root.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
       },
       {
         path: "loadspinner",
-        element: <LoadingSpiner />,
+        element: <LoadingSpinner />,
       },
       // {
       //   // path: "/about",
